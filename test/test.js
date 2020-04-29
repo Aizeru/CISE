f08ea42a35ec86af98c68b9e0841f74fff8979fb
Aizeru <53991455+Aizeru@users.noreply.github.com>
2020-04-29 18:34:53 +1200

    Mocha and Chai Testing

---
+++ test/test.js
@@ -0,0 +1,11 @@
+describe('Springfield', () => {
+	before(() => {
+		console.log('Marge calls Barge and Lisa (you see this only once)')
+	})
+	it('Bart should hear his mother', () => {
+		/** Test cases */
+	})
+	it('Lisa should hear her mother', () => {
+		/** Test cases */
+	})
+})

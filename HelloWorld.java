e6bcaad000c92de3363fb79eae67decaa5ba7b6e
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2020-04-29 14:56:26 +1200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args){
         //trash code
+        for(int i = 0; i < 3; i++){
         System.out.println("Hello World");
+        }
+        
     }
 
 }
